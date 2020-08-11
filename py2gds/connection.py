b964a55aaa2630f0e1a9b7c9b1bda1d17378a82a
Pablo Cabezas <pablocabezas@wegow.com>
2020-08-11 16:13:56 +0200

    Style

--- py2gds/connection.py
+++ py2gds/connection.py
@@ -15,7 +15,7 @@ class Neo4JDriverConnection(Connection):
     driver: Neo4jDriver
 
     @classmethod
-    def create(cls, uri: str, user: str, password: str, ) -> "Neo4JDriverConnection":
+    def create(cls, uri: str, user: str, password: str) -> "Neo4JDriverConnection":
         return cls(GraphDatabase.driver(uri, auth=(user, password)))
 
     def execute(self, query: str) -> List[Dict[str, Any]]:

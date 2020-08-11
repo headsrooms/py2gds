f120d0c368f87a49a364c5f61cdad071606cffb7
Pablo Cabezas <pablocabezas@wegow.com>
2020-08-11 16:19:07 +0200

    Add tests about skip, limit and order_by

--- tests/test_dsl.py
+++ tests/test_dsl.py
@@ -132,3 +132,79 @@ def test_query_order(graph_connection: Connection):
         == str(dsl_query_5)
         == str(dsl_query_6)
     )
+
+
+def test_sort_by_score_ascending(
+    graph_connection: Connection, pages_and_links_projection: Projection
+):
+    query = (
+        Query.using(graph_connection)
+        .rank(algorithm=AlgorithmType.ArticleRank)
+        .projected_by(labels=("Page",), relationships=("LINKS",))
+        .order_by("score")
+    )
+
+    results = query.run()
+    previous_score = None
+    for result in results:
+        if previous_score:
+            assert result["score"] >= previous_score
+        previous_score = result["score"]
+
+
+def test_sort_by_score_descending(
+    graph_connection: Connection, pages_and_links_projection: Projection
+):
+    query = (
+        Query.using(graph_connection)
+        .rank(algorithm=AlgorithmType.ArticleRank)
+        .projected_by(labels=("Page",), relationships=("LINKS",))
+        .order_by("score", descending=True)
+    )
+
+    results = query.run()
+    previous_score = None
+    for result in results:
+        if previous_score:
+            assert result["score"] <= previous_score
+        previous_score = result["score"]
+
+
+def test_limit(
+    graph_connection: Connection, pages_and_links_projection: Projection
+):
+    query = (
+        Query.using(graph_connection)
+        .rank(algorithm=AlgorithmType.ArticleRank)
+        .projected_by(labels=("Page",), relationships=("LINKS",))
+        .order_by("score", descending=True)
+        .limit(2)
+    )
+
+    results = query.run()
+    assert len(results) == 2
+
+
+def test_skip(
+    graph_connection: Connection, pages_and_links_projection: Projection
+):
+    normal_query = (
+        Query.using(graph_connection)
+        .rank(algorithm=AlgorithmType.ArticleRank)
+        .projected_by(labels=("Page",), relationships=("LINKS",))
+        .order_by("score", descending=True)
+        .limit(4)
+    )
+
+    skipped_query = (
+        Query.using(graph_connection)
+        .rank(algorithm=AlgorithmType.ArticleRank)
+        .projected_by(labels=("Page",), relationships=("LINKS",))
+        .order_by("score", descending=True)
+        .skip(3)
+        .limit(4)
+    )
+
+    normal_results = normal_query.run(log=True)
+    skipped_results = skipped_query.run(log=True)
+    assert normal_results[3]["score"] == skipped_results[0]["score"]
